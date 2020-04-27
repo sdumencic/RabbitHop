66fda473c6c8ad063a8794155e72d57450d6764e
stelladum <stella.dumencic@gmail.com>
2020-04-27 18:42:16 +0200

    Camera fixed

---
+++ Assets/Scripts/PlayerFollow2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow2 : MonoBehaviour{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate() {
+        Vector3 desiredPosition = 0.5f*target.position + offset;
+        Vector3 smoothenedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothenedPosition;
+    }
+}
